5ae44391bc1196e6880ff752907b096f65282758
Daniel Porrey <porrey@users.noreply.github.com>
2021-04-07 06:50:58 -0500

    In Progress

---
+++ Src/Virtual Printer Solution/VirtualPrinter/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using VirtualPrinter.ViewModels;
+
+namespace VirtualPrinter
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			this.InitializeComponent();
+			this.DataContext = new MainViewModel();
+		}
+	}
+}
