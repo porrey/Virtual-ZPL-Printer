a9279449697104b3e7ff8ac7755096627e5babdf
Daniel Porrey <porrey@users.noreply.github.com>
2021-04-08 06:50:51 -0500

    Added code.

---
+++ Src/Print Direct Example Solution/Print Direct Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static Task Main(string[] args)
+		{
+			return PrintDirect.SendUtf8TextAsync(@"\\localhost\4BARCODE", "Hello World");
+		}
+	}
+}
