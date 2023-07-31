38464233ab52149c4f623e753db2957416ae230b
Curd Becker <me@curd-becker.de>
2023-07-31 07:23:00 +0200

    Avoid randomly splitting large socket messages in multiple ZPL documents
    When receiving data from the socket, try to read everything from the
    socket using small chunks into a single buffer to give the client enough
    time to send (and us to receive) all data that it wants us to handle.
    This also allows to finally apply read timeouts to handle edge cases like
    unreliable network communication or slow clients.
    
    While the data could contain multiple ZPL documents, it could also only
    contain a single large ZPL document. In any case, there is no need to split
    received data, since Labelary and the code are capable of rendering and
    handling multiple ZPL documents.

--- Src/Virtual Printer Solution/VirtualPrinter/Hosted Services/TcpListenerClientHandler.cs
+++ Src/Virtual Printer Solution/VirtualPrinter/Hosted Services/TcpListenerClientHandler.cs
@@ -1,6 +1,6 @@
 ﻿/*
  *  This file is part of Virtual ZPL Printer.
- *  
+ *
  *  Virtual ZPL Printer is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
@@ -16,6 +16,7 @@
  */
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -57,67 +58,88 @@ namespace VirtualPrinter.Client
 			//
 			NetworkStream stream = client.GetStream();
 
-			while (client.Connected && stream.CanRead)
+			//
+			// Prepare a memory stream to read data into.
+			//
+			MemoryStream ms = new MemoryStream();
+
+			try
 			{
-				if (stream.DataAvailable)
+				if (client.Connected && stream.CanRead)
 				{
-					try
+					//
+					// Set up a temporary buffer.
+					//
+					byte[] data = new byte[1024];
+
+					//
+					// Read from the NetworkStream until there isn't any data to read available anymore.
+					//
+					// See https://stackoverflow.com/questions/26058594/how-to-get-all-data-from-networkstream
+					//
+					// Unfortunately, that doesn't mean that all data has necessarily been received from the client,
+					// so we could still haven't received some data after all, e.g. due to network delays. It would
+					// be better to try to read all data until the connection is terminated by the client, however,
+					// there isn't an easy way to detect a closed connection with a NetworkStream.
+					//
+					// By reading the data in small chunks, we're giving the client additional time to send
+					// data - the larger the data to be received, the more time available. Finally, by increasing
+					// the read timeout, we can further easily increase the time available to the client.
+					//
+					int numBytesRead;
+					while ((numBytesRead = stream.Read(data, 0, data.Length)) > 0)
 					{
-						//
-						// Create a buffer for the data that is available.
-						//
-						byte[] buffer = new byte[client.Available];
-
-						//
-						// Read the data into the buffer.
-						//
-						int bytesRead = await stream.ReadAsync(buffer.AsMemory(0, buffer.Length));
-
-						if (bytesRead > 0)
-						{
-							//
-							// Get the label image
-							//
-							string zpl = Encoding.UTF8.GetString(buffer);
-
-							if (!zpl.StartsWith("NOP"))
-							{
-								//
-								// Get the label images from Labelary.
-								//
-								IEnumerable<IGetLabelResponse> responses = await this.LabelService.GetLabelsAsync(labelConfiguration, zpl);
-
-								//
-								// Save the images.
-								//
-								IEnumerable<IStoredImage> storedImages = await this.ImageCacheRepository.StoreLabelImagesAsync(imagePathRoot, responses);
-
-								//
-								// Publish the images.
-								//
-								foreach (IGetLabelResponse response in responses)
-								{
-									//
-									// Publish the new label.
-									//
-									this.EventAggregator.GetEvent<LabelCreatedEvent>().Publish(new LabelCreatedEventArgs()
-									{
-										PrintRequest = new PrintRequestEventArgs()
-										{
-											LabelConfiguration = labelConfiguration,
-											Zpl = zpl
-										},
-										Label = storedImages.ElementAt(response.LabelIndex),
-										Result = response.Result,
-										Message = response.Result ? "Label successfully created." : response.Error
-									});
-								}
-							}
-						}
+					    ms.Write(data, 0, numBytesRead);
 					}
-					finally
+				}
+			}
+			finally
+			{
+				client.Close();
+			}
+
+			//
+			// Only try to create a label image if any data has been received in the first place.
+			//
+			if (ms.Length > 0)
+			{
+
+				//
+				// Get the label image.
+				//
+				string zpl = Encoding.UTF8.GetString(ms.ToArray(), 0, (int) ms.Length);
+
+				if (!zpl.StartsWith("NOP"))
+				{
+					//
+					// Get the label images from Labelary.
+					//
+					IEnumerable<IGetLabelResponse> responses = await this.LabelService.GetLabelsAsync(labelConfiguration, zpl);
+
+					//
+					// Save the images.
+					//
+					IEnumerable<IStoredImage> storedImages = await this.ImageCacheRepository.StoreLabelImagesAsync(imagePathRoot, responses);
+
+					//
+					// Publish the images.
+					//
+					foreach (IGetLabelResponse response in responses)
 					{
-						client.Close();
+						//
+						// Publish the new label.
+						//
+						this.EventAggregator.GetEvent<LabelCreatedEvent>().Publish(new LabelCreatedEventArgs()
+						{
+							PrintRequest = new PrintRequestEventArgs()
+							{
+								LabelConfiguration = labelConfiguration,
+								Zpl = zpl
+							},
+						Label = storedImages.ElementAt(response.LabelIndex),
+						Result = response.Result,
+						Message = response.Result ? "Label successfully created." : response.Error
+						});
 					}
 				}
 			}
